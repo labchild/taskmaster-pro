8e0e978a5c899316a2cb8971b1d742af146298f9
l bates childs <labchilds@gmail.com>
2021-11-10 19:03:13 -0500

    add custom styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,15 +160,19 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     $(this).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     $(this).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
   update: function(event) {
     // arr for temp storage of task data
@@ -227,7 +231,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
