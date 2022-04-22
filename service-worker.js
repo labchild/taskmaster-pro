0e7a8fd05bb005962801069f7427ed75b8c033e2
l bates childs <labchilds@gmail.com>
2022-04-22 18:31:26 -0400

    add service worker and function to register

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const APP_PREFIX = 'Taskmaster';
+const VERSION = 'v_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    './index.html',
+    './assets/css/style.css',
+    './assets/js/script.js'
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing chache: ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+// activate sw, delete any outdated resources
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// respond with cached resources
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request);
+        })
+    )
+})
